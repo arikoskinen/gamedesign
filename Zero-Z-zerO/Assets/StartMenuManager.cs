1639f00be125ff27adebde15985e6bb2c7e13da6
OPISTO\ari.koskinen <ari.koskinen@gmail.com>
2016-12-07 08:52:17 +0200

    Start Menu Added with Start button (Openst 1st stage when pressed)

---
+++ Zero-Z-zerO/Assets/StartMenuManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class StartMenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    public void StartGame() {
+
+        // Start Game here
+        SceneManager.LoadScene("Scenes/1st_Stage");  // remember to add this scene to build settings!
+        Debug.Log("StartGame button pressed");
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
